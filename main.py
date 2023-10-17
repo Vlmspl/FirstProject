8cfd2a657b25c8bdefeeca06cef5cab578123772
bilou <bilousvlad2020@gmail.com>
2023-10-17 19:50:06 +0300

    Homework 1, EvenNumber

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import math
+
+def CheckIfNumberIsEven(n):
+    if n/2 == math.floor(n/2):
+        return True
+    else:
+        return False
+
+
+a = int(input())
+
+print("Число", a, "є парним:", CheckIfNumberIsEven(a))
+
